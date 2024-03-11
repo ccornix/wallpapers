54dce9531fff0ad34276cc1b380f5fdc953f39ae
ccornix <ccornix1758@gmail.com>
2024-03-11 10:40:58 +0100

    refactor: revise generator scripts

--- scripts/hexagons.py
+++ scripts/hexagons.py
@@ -4,58 +4,54 @@ __author__ = "ccornix"
 __copyright__ = "Copyright (c) 2024 ccornix"
 __license__ = "MIT"
 
-from sympy import Rational, sqrt
+from sympy import sqrt
 import random
 
-from lib.grid import generate_grid
+from lib.grid import generate_grid, make_random_color_element_style_fn
 from lib.path import scale
 from lib.poly import regular_polygon_path
-from lib.svg import SVGPathStyle, generate_svg
+from lib.svg import generate_svg
 
 
 PALETTE = ["#202020", "#303030", "#404040", "#505050"]
+RESOLUTION = (1920, 1080)
+CELL_SIZE = (32, 36)
+SPACINGS = (CELL_SIZE[0], 3 * CELL_SIZE[1] // 4)
+ISLE = regular_polygon_path(6)
+ELEMENT_PATHS = [scale(ISLE, (CELL_SIZE[0] / sqrt(3), CELL_SIZE[1] // 2))]
 
 
-def element_style_fn(ix: int, iy: int) -> list[SVGPathStyle]:
-    """Return an SVG path style with a randomly selected fill color."""
-    return [
-        SVGPathStyle(
-            fill_color=PALETTE[random.randrange(1, len(PALETTE))],
-            stroke_color=PALETTE[0],
-            stroke_width=1,
-        )
-    ]
+def offsets_fn(ix: int, iy: int) -> tuple[int, int]:
+    """Shift each odd row of the hexagonal grid."""
+    return (CELL_SIZE[0] // 2 * (iy % 2), 0)
 
 
-if __name__ == "__main__":
+element_style_fn = make_random_color_element_style_fn(
+    fill_color=PALETTE[1:],
+    stroke_color=PALETTE[0],
+    stroke_width=1,
+)
+
+
+def main() -> None:  # noqa: D103
     random.seed(1)
-
-    resolution = (1920, 1080)
-
-    ax = 32
-    ay = 36
-    spacings = (ax, 3 * ay // 4)
-
-    def offsets_fn(ix: int, iy: int) -> tuple[int, int]:
-        return (ax // 2 * (iy % 2), 0)
-
-    hexagon = regular_polygon_path(6)
-
-    element_paths = [scale(hexagon, (ax / sqrt(3), Rational(ay, 2)))]
-
     print(
         generate_svg(
             author=__author__,
             title="Randomly colored hexagons",
             palette=PALETTE,
             background_color=PALETTE[0],
-            resolution=resolution,
+            resolution=RESOLUTION,
             paths=generate_grid(
-                element_paths=element_paths,
-                spacings=spacings,
+                element_paths=ELEMENT_PATHS,
+                spacings=SPACINGS,
                 offsets_fn=offsets_fn,
-                resolution=resolution,
+                resolution=RESOLUTION,
                 element_style_fn=element_style_fn,
             ),
         )
     )
+
+
+if __name__ == "__main__":
+    main()
--- scripts/kochflakes3.py
+++ scripts/kochflakes3.py
@@ -5,90 +5,79 @@ __copyright__ = "Copyright (c) 2024 ccornix"
 __license__ = "MIT"
 
 from sympy import Rational, pi, sqrt
-from typing import Callable
 import random
 
 from lib.koch import koch_island
-from lib.grid import generate_grid
+from lib.grid import generate_grid, make_random_color_element_style_fn
 from lib.path import scale, shift, rotate
-from lib.svg import SVGPathStyle, generate_svg
+from lib.svg import generate_svg
 
 
 PALETTE = ["#202020", "#303030", "#404040", "#505050"]
+RESOLUTION = (1920, 1080)
+CELL_SIZE = (64, 72)
+SPACINGS = (2 * CELL_SIZE[0], CELL_SIZE[1] // 2)
+ISLE = koch_island(iterations=3)
+BIG_ISLE = scale(ISLE, (CELL_SIZE[0] / sqrt(3), CELL_SIZE[1] // 2))
+SMALL_ISLE = scale(
+    rotate(ISLE, pi / 6),
+    (Rational(1, 3) * CELL_SIZE[0], Rational(1, 2) / sqrt(3) * CELL_SIZE[1]),
+)
+BIG_ELEMENT_PATHS = [BIG_ISLE]
+SMALL_ELEMENT_PATHS = [
+    shift(SMALL_ISLE, (-CELL_SIZE[0] * Rational(2, 3), 0)),
+    shift(SMALL_ISLE, (CELL_SIZE[0] * Rational(2, 3), 0)),
+]
 
 
-def make_element_style_fn(
-    count: int,
-) -> Callable[[int, int], list[SVGPathStyle]]:
-    """Make an element style factory function for `count` number of elements.
-
-    Return a function that produces a list of `count` number of SVG path
-    styles with a randomly selected fill color.
-    """
-
-    def element_style_fn(ix: int, iy: int) -> list[SVGPathStyle]:
-        return [
-            SVGPathStyle(
-                fill_color=PALETTE[random.randrange(1, len(PALETTE))],
-                stroke_color=PALETTE[0],
-                stroke_width=1,
-            )
-            for _ in range(count)
-        ]
-
-    return element_style_fn
+def offsets_fn(ix: int, iy: int) -> tuple[int, int]:
+    """Shift each odd row."""
+    return (CELL_SIZE[0] * (iy % 2), 0)
 
 
-if __name__ == "__main__":
+big_element_style_fn = make_random_color_element_style_fn(
+    fill_color=PALETTE[1:],
+    stroke_color=PALETTE[0],
+    stroke_width=1,
+)
+
+
+small_element_style_fn = make_random_color_element_style_fn(
+    fill_color=PALETTE[1:],
+    stroke_color=PALETTE[0],
+    stroke_width=1,
+    count=2,
+)
+
+
+def main() -> None:  # noqa: D103
     random.seed(1)
-
-    resolution = (1920, 1080)
-
-    ax = 64
-    ay = 72
-    spacings = (2 * ax, ay // 2)
-
-    def offsets_fn(ix: int, iy: int) -> tuple[int, int]:
-        return (ax * (iy % 2), 0)
-
-    koch_isle = koch_island(iterations=3)
-
-    big = scale(koch_isle, (ax / sqrt(3), Rational(1, 2) * ay))
-    big_element_paths = [big]
-    big_element_style_fn = make_element_style_fn(1)
-
-    small = scale(
-        rotate(koch_isle, pi / 6),
-        (Rational(1, 3) * ax, Rational(1, 2) / sqrt(3) * ay),
-    )
-    small_element_paths = [
-        shift(small, (-ax * Rational(2, 3), 0)),
-        shift(small, (ax * Rational(2, 3), 0)),
-    ]
-    small_element_style_fn = make_element_style_fn(2)
-
     print(
         generate_svg(
             author=__author__,
             title="Randomly colored Koch islands",
             palette=PALETTE,
             background_color=PALETTE[0],
-            resolution=resolution,
+            resolution=RESOLUTION,
             paths=(
                 generate_grid(
-                    element_paths=big_element_paths,
-                    spacings=spacings,
+                    element_paths=BIG_ELEMENT_PATHS,
+                    spacings=SPACINGS,
                     offsets_fn=offsets_fn,
-                    resolution=resolution,
+                    resolution=RESOLUTION,
                     element_style_fn=big_element_style_fn,
                 )
                 + generate_grid(
-                    element_paths=small_element_paths,
-                    spacings=spacings,
+                    element_paths=SMALL_ELEMENT_PATHS,
+                    spacings=SPACINGS,
                     offsets_fn=offsets_fn,
-                    resolution=resolution,
+                    resolution=RESOLUTION,
                     element_style_fn=small_element_style_fn,
                 )
             ),
         )
     )
+
+
+if __name__ == "__main__":
+    main()
